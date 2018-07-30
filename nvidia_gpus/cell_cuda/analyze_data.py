1f0018854cf55602168ed5e385518fcfe503847d
yuliana gomez <ygomez297@gmail.com>
2018-07-30 14:36:22 -0500

    combines data from all results files, plots data

--- nvidia_gpus/cell_cuda/analyze_data.py
+++ nvidia_gpus/cell_cuda/analyze_data.py
@@ -2,6 +2,10 @@
 import csv
 import numpy as np
 import os
+import glob
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import pandas as pd
 
 ###---Getting data from results and rates files -----###
 """
@@ -16,7 +20,7 @@ import os
     Splits given output - removes spaces, replaces with commas
     Each individual word is considered a column (start at 0)
 """
-def splitting_data():
+def splitting_data(f):
 
     lines = f.readlines()
     results = []
@@ -27,11 +31,12 @@ def splitting_data():
 
 """
     Getting average of column
-    {"Mem Total":0, "Mem Used":0, "Mem Free":0 ,"Power Drawed": 0, "Clocks": 0})
+    {"Mem Total":0, "Mem Used":2, "Mem Free":4 ,"Power Drawed": 6, "Clocks": 8})
 """
-def average_results():
-    results = np.array(splitting_data())
+def average_results(f):
+    results = np.array(splitting_data(f))
     data_dic = {}
+    #data_dic = []
     data_size = len(results)
     for col in range (0,10,2):
         total = 0
@@ -39,17 +44,51 @@ def average_results():
             total += float(y[1][col])
         average = total/data_size
 
-        data_dic[col] = average
+        data_dic[col] = (average)
+
+    #print (data_dic[2])
     return (data_dic)
 
 """
-   Plotting data from results file
+   Plotting data from *all* results file
+   {"Mem Total":0, "Mem Used":2, "Mem Free":4 ,"Power Drawed": 6, "Clocks": 8})
 """
 def data_analysis():
-    
+    files = glob.glob("*.results")
+    num = len(files)
+    all_data = defaultdict(list)
+
+    #Combining data from all files to one dictionary: all_data
+    for data in files:
+        onef_data = []
+        averages = {}
+        f = open(data,"r")
+        averages = average_results(f)
+        print("these are the averages: \n")
+        print (averages)
+        for k,v in averages.items():
+            all_data[k].append(v)
+    print ("total averages \n")
+    print (all_data)
+    x = all_data[2]
+    y = all_data[6]
+    plt.title("PIC(CUDA) Performance Results ")
+    plt.ylabel('Memory Used (MiB)')
+    plt.xlabel('Power Drawed (Watts)')
+    plt.plot(x,y)
+    plt.show()
+
+
+
+    #for k,v in mem_dic.items():
+    #    print (k,v)
+    #print (mem_dic[2])
+
+
 
 
 if __name__=="__main__":
     data_file = "gpu_clean.results"
-    f = open(data_file,"r")
-    average_results()
+    #f = open(data_file,"r")
+    #average_results()
+    data_analysis()
