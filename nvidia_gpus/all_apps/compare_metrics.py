81dba77ce9165b24f4216f2b698d317d404ecf20
yuliana gomez <ygomez297@gmail.com>
2018-09-18 12:43:43 -0500

    analysis of all metrics, needs to be tested on machine

--- nvidia_gpus/all_apps/compare_metrics.py
+++ nvidia_gpus/all_apps/compare_metrics.py
@@ -126,8 +126,10 @@ def compare_metrics(dict1,dict2):
     compared ={}
     #dict1 = combined_data_['hotspot_calculate_temp']
     #dict2 = combined_data_['huffman_vlc_encode_kernel_sm64huff']
-    diffkeys = [k for k in dict1 if dict1[k] != dict2[k]]
-
+    if x:
+        diffkeys = [k for k in dict1 if dict1[k] != dict2[k]]
+    else:
+        diffkeys = [k for k in dict1 if dict1[k] == dict2[k]]
     for k in diffkeys:
         compared[k] =  dict1[k], dict2[k],abs(dict1[k]-dict2[k])
 
@@ -137,13 +139,40 @@ def compare_all(large_dict):
     dif = {}
     for k,v in large_dict.items():
         for k2,v2 in large_dict.items():
-            dict_compared = str(k) + "." + str(k2)
-            dif[dict_compared] = compare_metrics(large_dict[k],large_dict[k2])
+            if k != k2:
+                dict_compared = str(k) + "." + str(k2)
+                dif[dict_compared] = compare_metrics(large_dict[k],large_dict[k2],x)
     return(dif)
 
+#acquiring list of metrics that have the highest differences in value
+def top_metrics(input_dic):
+    #use top_values only if values only are needed
+    #top_values = []
+    top_dic = []
+    for k,v in input_dic.items():
+        for k2,v2 in v.items():
+            if (v2[-1]) > 391601:
+                #print (k2, v2, k)
+                if min(top_values) < v2[-1]:
+                    if len(top_values) == 10:
+                        #top_values.remove(min(top_values))
+                        #top_dic.remove(min(top_dic))
+                    #top_values.append(v2[-1])
+                top_dic.append((k,k2,v2[-1]))
+    #print(sorted(top_values))
+    #print(top_dic)
+    list_sorted = (sorted(top_dic, key=lambda tup:tup[2],reverse=True))
+    return (list_sorted)
 
+def metric_analysis(dict):
+    top10 = list_sorted[:30]
+    for c in top10:
+        print(c[0].split('.')[0])
 
 def main():
     combined_apps = single_run()
-    compared_metrics_dic = compare_all(combined_apps)
-    return compared_metrics_dic
+    diff_values = compare_all(combined_apps,True)
+    same_values = compare_all(combined_apps,False)
+    sorted_list = top_metrics(diff_values)
+    metric_analysis[sorted_list]
+    #return compared_metrics_dic
